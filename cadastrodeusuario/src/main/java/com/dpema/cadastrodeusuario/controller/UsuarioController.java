5e4871a23ac9e7df0aa40eea4df506e6b27404b2
Jeff-jp98 <57569798+Jeff-jp98@users.noreply.github.com>
2020-10-16 16:56:48 -0300

    lançamento de comandos.

--- cadastrodeusuario/src/main/java/com/dpema/cadastrodeusuario/controller/UsuarioController.java
+++ cadastrodeusuario/src/main/java/com/dpema/cadastrodeusuario/controller/UsuarioController.java
@@ -1,5 +1,10 @@
 package com.dpema.cadastrodeusuario.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +20,10 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository repository;
+	
+	static final Runtime run = Runtime.getRuntime();
+	static Process pro;
+	static BufferedReader read;
 	
 	@RequestMapping("/cadastro")
 	public String form(Model model){
@@ -32,5 +41,37 @@ public class UsuarioController {
 	public String listar(Model model) {
 		model.addAttribute("usuarios", repository.findAll());
 		return "lista";
+	}
+
+	@RequestMapping("/ipconfig")
+	public String ipconfig(Model model) throws Exception {
+		String[] cmds = {
+	            "ipconfig"
+	        };
+
+	        try {
+	            ProcessBuilder builder = new ProcessBuilder("cmd", "/c",
+	                String.join("& ", cmds));
+
+	            builder.redirectErrorStream(true);
+
+	            Process p = builder.start();
+
+	            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	            String line;
+
+	            while (true) {
+	                line = r.readLine();
+	                if (line == null) {
+	                    break;
+	                }
+
+	                model.addAttribute(line);
+	                System.out.println(line);
+	            }
+	        } catch(Exception e) {
+	            System.err.println(e);
+	        }
+		return "resposta";
 	}
 }
